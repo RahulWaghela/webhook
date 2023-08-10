248c069e8f2c03645e39f24a26d5232e6913533f
Rahul Waghela <rahulwaghela191919@gmail.com>
2023-08-10 19:04:18 +0530

    change new code

--- index.js
+++ index.js
@@ -1,215 +1,61 @@
-const express = require('express')
-const body_parser = require('body-parser')
-const axios = require('axios')
+'use strict';
 
-const app = express().use(body_parser.json())
-const port = process.env.PORT || 8000
-const my_token = process.env.MYTOKEN
-const API_token = process.env.API_TOKEN
-
-const BulkSMS =
-  'We are no.1 Bulk SMS service provider in India. Please select the service you want to apply for.Bulk  \n\n1. Promotional SMS \n2. Transactional SMS \n3. Sender ID'
-const PromotionalSMS_1 =
-  'You have selected option\n1. Promotional SMS\n\nPlease select the Quantity option.\n\nReply with option name or number (2.1 or 2.3 or 1 lack)\n\n1.1. Less then 1 lack \n1.2. 1-5 lacks \n1.3. 5-10 lacks \n1.4. More then 10 lacks'
-const TransactionalSMS_2 =
-  'You have selected option\n2. Transactional SMS\n\nPlease select the Quantity option.\n\nReply with option name or number (2.1 or 2.3 or 1 lack)\n\n2.1. Less then 1 lack\n2.2. 1-5 lacks\n2.3. 5-10 lacks\n2.4. More then 10 lacks'
-const Last_mess = `Thanks you for your reply.😊\n\nOur support executive will get back to you very soon.`
-
-app.get('/', (req, res) => {
-  res.send('Webhook')
-})
+// Imports dependencies and set up http server
+const
+  express = require('express'),
+  bodyParser = require('body-parser'),
+  app = express().use(bodyParser.json()); // creates express http server
+// Creates the endpoint for our webhook 
+app.post('/webhook', (req, res) => {  
+ 
+    let body = req.body;
+  
+    // Checks this is an event from a page subscription
+    if (body.object === 'page') {
+  
+      // Iterates over each entry - there may be multiple if batched
+      body.entry.forEach(function(entry) {
+  
+        // Gets the message. entry.messaging is an array, but 
+        // will only ever contain one message, so we get index 0
+        let webhook_event = entry.messaging[0];
+        console.log(webhook_event);
+      });
+  
+      // Returns a '200 OK' response to all requests
+      res.status(200).send('EVENT_RECEIVED');
+    } else {
+      // Returns a '404 Not Found' if event is not from a page subscription
+      res.sendStatus(404);
+    }
+  
+  });
+  // Adds support for GET requests to our webhook
 app.get('/webhook', (req, res) => {
-  let mode = req.query['hub.mode']
-  let challenge = req.query['hub.challenge']
-  let token = req.query['hub.verify_token']
 
-  if (mode && token) {
-    if (mode === 'subscribe' && token === my_token) {
-      res.status(200).send(challenge)
-    } else {
-      res.status(403)
-    }
-  } else {
-    res.status(403)
-  }
-})
-
-app.post('/webhook', (req, res) => {
-  let bodyMess = req.body
-  console.log(JSON.stringify(bodyMess, null, 2))
-  if (bodyMess.object) {
-    console.log('bodyMess if in :-', JSON.stringify(bodyMess))
-    if (
-      bodyMess.entry &&
-      bodyMess.entry[0].changes &&
-      bodyMess.entry[0].changes[0].value.messages &&
-      bodyMess.entry[0].changes[0].value.messages[0]
-    ) {
-      let phone_number_id =
-        bodyMess.entry[0].changes[0].value.metadata.phone_number_id
-      let from = bodyMess.entry[0].changes[0].value.messages[0].from
-      let mess = bodyMess.entry[0].changes[0].value.messages[0].text.body
-
-      console.log('phone_number_id', phone_number_id)
-      console.log('from', from)
-      console.log('mess', mess)
-
-      let Replay
-
-      switch (mess) {
-        case 'Promotional SMS':
-          Replay = PromotionalSMS_1
-          break
-        case '1. Promotional SMS':
-          Replay = PromotionalSMS_1
-          break
-        case '1':
-          Replay = PromotionalSMS_1
-          break
-        case 'Transactional SMS':
-          Replay = TransactionalSMS_2
-          break
-        case '2. Transactional SMS':
-          Replay = TransactionalSMS_2
-          break
-        case '2':
-          Replay = TransactionalSMS_2
-          break
-        case 'Sender ID':
-          Replay = Last_mess
-          break
-        case '3. Sender ID':
-          Replay = Last_mess
-          break
-        case '3':
-          Replay = Last_mess
-          break
-        case '2.1. Less then 1 lack':
-          Replay = Last_mess
-          break
-        case '2.2. 1-5 lacks':
-          Replay = Last_mess
-          break
-        case '2.3. 5-10 lacks':
-          Replay = Last_mess
-          break
-        case '2.4. More then 10 lacks':
-          Replay = Last_mess
-          break
-        case '1.1. Less then 1 lack':
-          Replay = Last_mess
-          break
-        case '1.2. 1-5 lacks':
-          Replay = Last_mess
-          break
-        case '1.3. 5-10 lacks':
-          Replay = Last_mess
-          break
-        case '1.4. More then 10 lacks':
-          Replay = Last_mess
-          break
-        case 'Less then 1 lack':
-          Replay = Last_mess
-          break
-        case '1-5 lacks':
-          Replay = Last_mess
-          break
-        case '5-10 lacks':
-          Replay = Last_mess
-          break
-        case 'More then 10 lacks':
-          Replay = Last_mess
-          break
-        case '2.1':
-          Replay = Last_mess
-          break
-        case '2.2':
-          Replay = Last_mess
-          break
-        case '2.3':
-          Replay = Last_mess
-          break
-        case '2.4':
-          Replay = Last_mess
-          break
-        case '1.1':
-          Replay = Last_mess
-          break
-        case '1.2':
-          Replay = Last_mess
-          break
-        case '1.3':
-          Replay = Last_mess
-          break
-        case '1.4':
-          Replay = Last_mess
-          break
-        case '2.1.':
-          Replay = Last_mess
-          break
-        case '2.2.':
-          Replay = Last_mess
-          break
-        case '2.3.':
-          Replay = Last_mess
-          break
-        case '2.4.':
-          Replay = Last_mess
-          break
-        case '1.1.':
-          Replay = Last_mess
-          break
-        case '1.2.':
-          Replay = Last_mess
-          break
-        case '1.3.':
-          Replay = Last_mess
-          break
-        case '1.4.':
-          Replay = Last_mess
-          break
-        default:
-          Replay = BulkSMS
-          break
+    // Your verify token. Should be a random string.
+    let VERIFY_TOKEN = process.env.VERIFY_TOKEN
+      
+    // Parse the query params
+    let mode = req.query['hub.mode'];
+    let token = req.query['hub.verify_token'];
+    let challenge = req.query['hub.challenge'];
+      
+    // Checks if a token and mode is in the query string of the request
+    if (mode && token) {
+    
+      // Checks the mode and token sent is correct
+      if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+        
+        // Responds with the challenge token from the request
+        console.log('WEBHOOK_VERIFIED');
+        res.status(200).send(challenge);
+      
+      } else {
+        // Responds with '403 Forbidden' if verify tokens do not match
+        res.sendStatus(403);      
       }
-      const Data = JSON.stringify({
-        messaging_product: 'whatsapp',
-        recipient_type: 'individual',
-        to: from,
-        type: 'text',
-        text: {
-          preview_url: false,
-          body: Replay,
-        },
-      })
-
-      var config = {
-        method: 'post',
-        url: `https://graph.facebook.com/v15.0/${phone_number_id}/messages`,
-        headers: {
-          authorization: `Bearer ${API_token}`,
-          'Content-Type': 'application/json',
-        },
-        data: Data,
-      }
-
-      axios(config)
-        .then(function (response) {
-          console.log(JSON.stringify('response', response.data))
-          res.sendStatus(200)
-        })
-        .catch(function (error) {
-          console.log('error', error)
-          res.sendStatus(403)
-        })
-    } else {
-      res.sendStatus(403)
     }
-  } else {
-    res.sendStatus(403)
-  }
-})
-
-app.listen(port, () => {
-  console.log('webhook is listening on srver')
-})
+  });
+// Sets server port and logs message on success
+app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
