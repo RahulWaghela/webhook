be766bdef35b949aa0344deff70de5ac60315ec4
Rahul Waghela <rahulwaghela191919@gmail.com>
2023-08-10 19:59:35 +0530

    little

--- index.js
+++ index.js
@@ -29,12 +29,12 @@ app.post('/webhook', (req, res) => {
     if (body_param.object) {
         if (body_param.entry &&
             body_param.entry[0].changes &&
-            body_param.entry[0].changes.value.message &&
-            body_param.entry[0].changes[0].value.message[0]) {
+            body_param.entry[0].changes.value.messages &&
+            body_param.entry[0].changes[0].value.messages[0]) {
 
             let phon_no_id = body_param.entry[0].changes[0].value.metadata.phone_number_id;
-            let from = body_param.entry[0].changes[0].value.message[0].from;
-            let msg_body = body_param.entry[0].changes[0].value.message[0].text.body;
+            let from = body_param.entry[0].changes[0].value.messages[0].from;
+            let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
             //add for testing only
             const Data = JSON.stringify({
                 messaging_product: 'whatsapp',
