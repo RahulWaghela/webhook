dc77a0ced7961ea84dfa766ffb9106b0c0bbb045
Rahul Waghela <rahulwaghela191919@gmail.com>
2023-08-10 19:28:33 +0530

    sdsdssd

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.get('/webhook', (req, res) => {
     let mode = req.query['hub.mode'];
     let challenge = req.query['hub.challenge'];
     let token = req.query['hub.verify_token'];
+    
     if (mode && token) {
         if (mode === 'subscribe' && token === mytoken) {
             res.status(200).send(challenge);
@@ -21,7 +22,9 @@ app.get('/webhook', (req, res) => {
 });
 
 app.post('/webhook', (req, res) => {
+
     let body_param = req.body;
+
     console.log(JSON.stringify(body_param, null, 2));
     if (body_param.object) {
         if (body_param.entry &&
@@ -32,11 +35,11 @@ app.post('/webhook', (req, res) => {
             let phon_no_id = body_param.entry[0].changes[0].value.metadata.phone_number_id;
             let from = body_param.entry[0].changes[0].value.message[0].from;
             let msg_body = body_param.entry[0].changes[0].value.message[0].text.body;
-            // add for testing only
-            const data = JSON.stringify({
+            //add for testing only
+            const Data = JSON.stringify({
                 messaging_product: 'whatsapp',
                 recipient_type: 'individual',
-                to: 9726629466,
+                to: 9157808228,
                 type: 'text',
                 text: {
                   preview_url: false,
@@ -58,11 +61,11 @@ app.post('/webhook', (req, res) => {
                     "Authorization":`Bearer ${process.env.TOKEN}`,
                     "Content-Type": "application/json"
                 },
-                data:data
+                data:Data
             });
-            res.sendStatus(200);
+            res.send(200);
         } else {
-            res.sendStatus(404); 
+            res.send(404);
         }
     }
 });
