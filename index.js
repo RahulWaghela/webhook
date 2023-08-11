d69669683b4a00fb5a4013eea54598d61b0db8bc
Rahul Waghela <rahulwaghela191919@gmail.com>
2023-08-11 13:59:03 +0530

    code has been replaced new one

--- index.js
+++ index.js
@@ -1,67 +1,215 @@
-const express=require("express");
-const body_parser=require("body-parser");
-const axios=require("axios");
-require('dotenv').config();
-const app=express().use(body_parser.json());
-const token=process.env.TOKEN;
-const mytoken=process.env.MYTOKEN;
-app.listen(process.env.PORT,()=>{
-    console.log("webhook is listening");
-});
-//to verify the callback url from dashboard side - cloud api side
-app.get("/webhook",(req,res)=>{
-   let mode=req.query["hub.mode"];
-   let challange=req.query["hub.challenge"];
+const express = require('express')
+const body_parser = require('body-parser')
+const axios = require('axios')
 
-   
+const app = express().use(body_parser.json())
+const port = process.env.PORT || 8000
+const my_token = process.env.MYTOKEN
+const API_token = process.env.API_TOKEN
 
-   let token=req.query["hub.verify_token"];
-    if(mode && token){
-        if(mode==="subscribe" && token===mytoken){
-            res.status(200).send(challange);
-        }else{
-            res.status(403);
-        }
+const BulkSMS =
+  'We are no.1 Bulk SMS service provider in India. Please select the service you want to apply for.Bulk  \n\n1. Promotional SMS \n2. Transactional SMS \n3. Sender ID'
+const PromotionalSMS_1 =
+  'You have selected option\n1. Promotional SMS\n\nPlease select the Quantity option.\n\nReply with option name or number (2.1 or 2.3 or 1 lack)\n\n1.1. Less then 1 lack \n1.2. 1-5 lacks \n1.3. 5-10 lacks \n1.4. More then 10 lacks'
+const TransactionalSMS_2 =
+  'You have selected option\n2. Transactional SMS\n\nPlease select the Quantity option.\n\nReply with option name or number (2.1 or 2.3 or 1 lack)\n\n2.1. Less then 1 lack\n2.2. 1-5 lacks\n2.3. 5-10 lacks\n2.4. More then 10 lacks'
+const Last_mess = `Thanks you for your reply.😊\n\nOur support executive will get back to you very soon.`
+
+app.get('/', (req, res) => {
+  res.send('Webhook')
+})
+app.get('/webhook', (req, res) => {
+  let mode = req.query['hub.mode']
+  let challenge = req.query['hub.challenge']
+  let token = req.query['hub.verify_token']
+
+  if (mode && token) {
+    if (mode === 'subscribe' && token === my_token) {
+      res.status(200).send(challenge)
+    } else {
+      res.status(403)
     }
-});
-app.post("/webhook",(req,res)=>{ //i want some 
-    let body_param=req.body;
-    console.log(JSON.stringify(body_param,null,2));
-    if(body_param.object){
-        console.log("inside body param");
-        if(body_param.entry && 
-            body_param.entry[0].changes && 
-            body_param.entry[0].changes[0].value.messages && 
-            body_param.entry[0].changes[0].value.messages[0]  
-            ){
-               let phon_no_id=body_param.entry[0].changes[0].value.metadata.phone_number_id;
-               let from = body_param.entry[0].changes[0].value.messages[0].from; 
-               let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
-               console.log("phone number "+phon_no_id);
-               console.log("from "+from);
-               console.log("boady param "+msg_body);
+  } else {
+    res.status(403)
+  }
+})
 
-               axios({
-                   method:"POST",
-                   url:"https://graph.facebook.com/v13.0/"+phon_no_id+"/messages?access_token="+token,
-                   data:{
-                       messaging_product:"whatsapp",
-                       to:from,
-                       text:{
-                           body:"Hi.., your message is "+msg_body
-                       }
-                   },
-                   headers:{
-                       "Content-Type":"application/json"
-                   }
-               });
-               res.sendStatus(200);
-            }else{
-                res.sendStatus(404);
-            }
+app.post('/webhook', (req, res) => {
+  let bodyMess = req.body
+  console.log(JSON.stringify(bodyMess, null, 2))
+  if (bodyMess.object) {
+    console.log('bodyMess if in :-', JSON.stringify(bodyMess))
+    if (
+      bodyMess.entry &&
+      bodyMess.entry[0].changes &&
+      bodyMess.entry[0].changes[0].value.messages &&
+      bodyMess.entry[0].changes[0].value.messages[0]
+    ) {
+      let phone_number_id =
+        bodyMess.entry[0].changes[0].value.metadata.phone_number_id
+      let from = bodyMess.entry[0].changes[0].value.messages[0].from
+      let mess = bodyMess.entry[0].changes[0].value.messages[0].text.body
+
+      console.log('phone_number_id', phone_number_id)
+      console.log('from', from)
+      console.log('mess', mess)
+
+      let Replay
+
+      switch (mess) {
+        case 'Promotional SMS':
+          Replay = PromotionalSMS_1
+          break
+        case '1. Promotional SMS':
+          Replay = PromotionalSMS_1
+          break
+        case '1':
+          Replay = PromotionalSMS_1
+          break
+        case 'Transactional SMS':
+          Replay = TransactionalSMS_2
+          break
+        case '2. Transactional SMS':
+          Replay = TransactionalSMS_2
+          break
+        case '2':
+          Replay = TransactionalSMS_2
+          break
+        case 'Sender ID':
+          Replay = Last_mess
+          break
+        case '3. Sender ID':
+          Replay = Last_mess
+          break
+        case '3':
+          Replay = Last_mess
+          break
+        case '2.1. Less then 1 lack':
+          Replay = Last_mess
+          break
+        case '2.2. 1-5 lacks':
+          Replay = Last_mess
+          break
+        case '2.3. 5-10 lacks':
+          Replay = Last_mess
+          break
+        case '2.4. More then 10 lacks':
+          Replay = Last_mess
+          break
+        case '1.1. Less then 1 lack':
+          Replay = Last_mess
+          break
+        case '1.2. 1-5 lacks':
+          Replay = Last_mess
+          break
+        case '1.3. 5-10 lacks':
+          Replay = Last_mess
+          break
+        case '1.4. More then 10 lacks':
+          Replay = Last_mess
+          break
+        case 'Less then 1 lack':
+          Replay = Last_mess
+          break
+        case '1-5 lacks':
+          Replay = Last_mess
+          break
+        case '5-10 lacks':
+          Replay = Last_mess
+          break
+        case 'More then 10 lacks':
+          Replay = Last_mess
+          break
+        case '2.1':
+          Replay = Last_mess
+          break
+        case '2.2':
+          Replay = Last_mess
+          break
+        case '2.3':
+          Replay = Last_mess
+          break
+        case '2.4':
+          Replay = Last_mess
+          break
+        case '1.1':
+          Replay = Last_mess
+          break
+        case '1.2':
+          Replay = Last_mess
+          break
+        case '1.3':
+          Replay = Last_mess
+          break
+        case '1.4':
+          Replay = Last_mess
+          break
+        case '2.1.':
+          Replay = Last_mess
+          break
+        case '2.2.':
+          Replay = Last_mess
+          break
+        case '2.3.':
+          Replay = Last_mess
+          break
+        case '2.4.':
+          Replay = Last_mess
+          break
+        case '1.1.':
+          Replay = Last_mess
+          break
+        case '1.2.':
+          Replay = Last_mess
+          break
+        case '1.3.':
+          Replay = Last_mess
+          break
+        case '1.4.':
+          Replay = Last_mess
+          break
+        default:
+          Replay = BulkSMS
+          break
+      }
+      const Data = JSON.stringify({
+        messaging_product: 'whatsapp',
+        recipient_type: 'individual',
+        to: from,
+        type: 'text',
+        text: {
+          preview_url: false,
+          body: Replay,
+        },
+      })
+
+      var config = {
+        method: 'post',
+        url: `https://graph.facebook.com/v15.0/${phone_number_id}/messages`,
+        headers: {
+          authorization: `Bearer ${API_token}`,
+          'Content-Type': 'application/json',
+        },
+        data: Data,
+      }
+
+      axios(config)
+        .then(function (response) {
+          console.log(JSON.stringify('response', response.data))
+          res.sendStatus(200)
+        })
+        .catch(function (error) {
+          console.log('error', error)
+          res.sendStatus(403)
+        })
+    } else {
+      res.sendStatus(403)
     }
+  } else {
+    res.sendStatus(403)
+  }
+})
 
-});
-app.get("/",(req,res)=>{
-    res.status(200).send("hello this is webhook setup");
-});
+app.listen(port, () => {
+  console.log('webhook is listening on srver')
+})
